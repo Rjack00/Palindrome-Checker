b9ee6075ec76d67ebcdb8b5e605a5d457f68025f
Rjack00 <rjackson77@comcast.net>
2024-06-07 19:32:48 -0600

    Updated html, css and js
    html: added input and button; css: basic styling; js: added incomplete function and document.getElementById for input and button, also eventListener for button and incomplete isItAPalindrome function

---
+++ script.js
@@ -0,0 +1,17 @@
+const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const userText = textInput.value;
+
+const isItAPalindrome = (text) => {
+    const regex = /\w/;
+    let cleanText = text.match(regex);
+
+    if(text.length < 1) {
+        return alert("Testing: " + text);
+    };
+    if(text.length > 0) {
+        return alert("Input Value: " + text);
+    };
+}
+
+checkButton.addEventListener("click", () => {alert(`Text value: ${userText}`)});
