aab2db3e423859ecae890b5578a42e39988e0317
Rjack00 <rjackson77@comcast.net>
2024-06-11 02:23:46 -0600

    Updated html js and css
    added final styling and working js code for app

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 const textInput = document.getElementById("text-input");
 const checkButton = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
 
 const isItAPalindrome = (text) => {
-    const regex = /\w/;
-    let cleanText = text.match(regex);
-
     if(text.length < 1) {
-        return alert("Testing: " + text);
-    };
-    if(text.length > 0) {
-        return alert("Input Value: " + text);
+        return alert(`Please input a value` );
+    } 
+
+    const regex = /[a-z0-9]/ig;
+    const cleanText = text.toLowerCase().match(regex).join("");
+    const reverseText = cleanText.split("").reverse().join("");
+
+    if(cleanText === reverseText) {
+        return result.innerText = `${text} is a palindrome`;
+
+    } else {
+        return result.innerText = `${text} is not a palindrome`;
     };
 }
 
+
 checkButton.addEventListener("click", () => {
-    alert(`Text value: ${textInput.value}`);
+    return isItAPalindrome(textInput.value);
 });
+
